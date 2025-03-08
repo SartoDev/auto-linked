e20262533bf72c223cc5b6a0cb6be2c26a692679
Leonardo Sarto <leonardosarto1@gmail.com>
2025-03-08 01:14:10 -0300

    Changed card color

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -175,7 +175,7 @@ const ChatPageSlug = () => {
           </div>
         </header>
 
-        <ScrollArea className="rounded-md border">
+        <ScrollArea>
           <div ref={chatContainerRef} className="flex-1 overflow-y-auto p-4 space-y-4">
             {messages.map((message) => (
                 <div className="gap-1 flex flex-col">
